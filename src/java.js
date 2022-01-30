6ebf5ac5cb71d36071c83bd3d8948b03817d8f10
Veronika1007 <veronika.horatschek@gmail.com>
2022-01-30 17:31:59 +0000

    Added Axios to get temp elements

---
+++ src/java.js
@@ -0,0 +1,43 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dateTime = document.querySelector("#date");
+dateTime.innerHTML = `${day}, ${hour}:${minutes}`;
+
+function displayTemperature(response) {
+  let tempElement = document.querySelector("#temperature");
+  tempElement.innerHTML = `Current Temperature is ${Math.round(
+    response.data.main.temp
+  )} ℃`;
+  let feelsLike = document.querySelector("#feelsLike");
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
+  feelsLike.innerHTML = `${Math.round(response.data.main.feels_like)} ℃`;
+  console.log(response.data);
+  description.innerHTML = response.data.weather[0].description;
+  humidity.innerHTML = response.data.main.humidity;
+  wind.innerHTML = `${Math.round(response.data.wind.speed)}km/hr`;
+}
+
+let apiKey = "f9113edd4d5c19caba9923a536e8e53e";
+let cityInput = "london";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
